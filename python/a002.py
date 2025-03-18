e3c0e10e17e5ac20e997e38632b18458eda0cdff
RyanLai <ryan.yonglai@gmail.com>
2025-03-18 14:41:24 +0800

    Version2

--- python/a002.py
+++
@@ -1,6 +0,0 @@
-numbers = str(input())
-numbers = numbers.split(" ")
-summ = 0
-for num in numbers :
-    summ += int(num)
-print(summ)
--- python/a022.py
+++
@@ -1,7 +0,0 @@
-a = list(str(input()))
-reverse_a = list(reversed(a))
-
-if a == reverse_a:
-    print("yes")
-else :
-    print("no")
